e1d02de60610596de2435d26793e141b53f20e51
mawafaqu <63239367+mawafaqu@users.noreply.github.com>
2020-10-31 17:25:53 +0700

    Add files via upload

---
+++ IF1-10117126-Latihan18-Kelipatan/src/if1/pkg10117126/latihan18/kelipatan/IF110117126Latihan18Kelipatan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10117126.latihan18.kelipatan;
+
+/**
+ *
+ * NAMA :Mawa Faqu Rochman 
+ * KELAS: IF-1 
+ * NIM : 10117126 
+ * Deskripsi Program: Program ini berisi program perulangan yang menampilkan 
+ * kelipatan angka 3.5 secara ascending
+ */
+public class IF110117126Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (double i = 3.5; i <= 35; i += 3.5) {
+            System.out.println(i);
+        }
+
+    }
+}
